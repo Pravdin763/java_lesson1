ec1ec6ef0df2543953887c20c38b66c98a48a972
igor <igor.pravdin163@gmail.com>
2023-01-23 12:47:48 +0400

    3

---
+++ lesson1_3.java
@@ -0,0 +1,21 @@
+public class lesson1_3
+{
+    public static void transArr(int [] arr, int k)
+    {
+        int i_f,i_t;
+        i_t=0;
+        for (i_f=0; i_f<arr.length; i_f++)
+            if (arr[i_f] !=k ) arr[i_t++]=arr[i_f];
+        for (i_f=i_t; i_f<arr.length; i_f++) arr[i_f]=k;    
+    }
+    
+    public static void main(String[] args)
+    {
+        int[] b = {3,2,2,3};
+        for (int x: b) System.out.print(x+" ");
+        System.out.println();
+        transArr(b,3);
+        for (int x: b) System.out.print(x+" ");
+        System.out.println();
+    }
+}
